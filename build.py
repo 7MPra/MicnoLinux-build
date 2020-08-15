3e415f980177d4e019b1e37342667fc92a757185
TMP_tenpura <tmptenpura@gmail.com>
2020-08-15 08:36:27 +0000

    update

--- build.py
+++ build.py
@@ -13,22 +13,13 @@ def index_Multi(List,liter):
 def is_use_shell(cmd):
 	return cmd == 'deb' or cmd == 'apt' or cmd == 'pge' or cmd == 'run'
 
-if "-s" in sys.argv:
+if "-f" in sys.argv:
 	filesystem = sys.argv[sys.argv.index("-s")+1]
-elif "--squashfs" in sys.argv:
+elif "--filesystem" in sys.argv:
 	filesystem = sys.argv[sys.argv.index("--squashfs")+1]
 else:
 	print("{} : squash filesystem file not specified")
 	sys.exit(1)
-
-if "-t" in sys.argv:
-	desktoptheme = sys.argv[sys.argv.index("-t")+1]
-	print("selected desktop theme {}".format(desktoptheme))
-elif "--theme" in sys.argv:
-	desktoptheme = sys.argv[sys.argv.index("--theme")+1]
-	print("selected desktop theme {}".format(desktoptheme))
-else:
-	desktoptheme = None
 
 if "-p" in sys.argv:
 	plymouththeme = sys.argv[sys.argv.index("-p")+1]
@@ -39,16 +30,17 @@ elif "--plymouth" in sys.argv:
 else:
 	plymouththeme = None
 
-if "-f" in sys.argv:
+if "-s" in sys.argv:
 	flow_path = sys.argv[sys.argv.index("-f")+1]
-elif "--flow" in sys.argv:
+elif "--script" in sys.argv:
 	flow_path = sys.argv[sys.argv.index("--flow")+1]
 else:
-	print("{} : squash filesystem file not specified")
+	print("{} : script file not specified")
 	sys.exit(1)
 
 flow = []
 with open(flow_path) as f:
+	beforecmd = None
 	l_strip = [s.strip() for s in f.readlines()]
 	for s in l_strip:
 		cmd = s[:3]
@@ -115,18 +107,18 @@ with open(flow_path) as f:
 			del args
 			del purgename
 		if cmd == "run":
-			runcommand = s[3:]
+			runcommand = s[4:]
 			if is_use_shell(beforecmd):
 				flow[len(flow)-1].append([4,runcommand])
 			else:
 				flow.append([3,[4,runcommand]])
 			print("run \"{}\"".format(runcommand))
-			del args
 			del runcommand
 		beforecmd = s[:3]
 del flow_path
 del cmd
 del beforecmd
+print(flow)
 print("now unpacking {}".format(filesystem))
 os.system("unsquashfs {}".format(filesystem))
 
@@ -135,14 +127,10 @@ with open("./squashfs-root/etc/resolv.conf","w") as f:
 	f.write("nameserver 1.1.1.1\n")
 	f.write("nameserver 8.8.8.8\n")
 
-with open("runsquashfs.sh","w") as f:
+with open("./squashfs-root/tmp/runsquashfs.sh","w") as f:
 	f.write("#!/bin/bash\n")
 	f.write("sudo apt-get update\n")
 	f.write("sudo apt-get install -y gdebi\n")
-	if desktoptheme != None:
-		print("copy {} to ./squashfs-root/usr/share/desktop-base/{}".format(desktoptheme,pathlib.Path(desktoptheme).name))
-		os.system("cp -r {} ./squashfs-root/usr/share/desktop-base/".format(desktoptheme))
-		f.write("update-alternatives --install /usr/share/desktop-base/active-theme desktop-theme /usr/share/desktop-base/{} 100\n".format(pathlib.Path(desktoptheme).name))
 	if plymouththeme != None:
 		print("copy {} to ./squashfs-root/usr/share/plymouth/themes/{}".format(plymouththeme,pathlib.Path(plymouththeme).name))
 		os.system("cp -r {} ./squashfs-root/usr/share/plymouth/themes/".format(plymouththeme))
@@ -150,22 +138,25 @@ with open("runsquashfs.sh","w") as f:
 		f.write("plymouth-set-default-theme {} -R\n".format(pathlib.Path(plymouththeme).name))
 		f.write("update-initramfs -u -k all\n")
 
-del desktoptheme
+os.system("sudo chroot squashfs-root/ bash /tmp/runsquashfs.sh")
+
 del plymouththeme
 
 for r in flow:
+	print(r)
 	if r[0] == 1:
 		os.system("cp -r {} ./squashfs-root{}".format(r[1],r[2]))
 		print("copy {} to {}".format(r[1],r[2]))
 	if r[0] == 2:
 		if os.path.exists("./squashfs-root{}".format(r[1])):
 			os.system("rm -r ./squashfs-root{}".format(r[1]))
-			print("remove {}".format(r[1])))
+			print("remove {}".format(r[1]))
 		else:
 			print("{} : {} : no such file or directory".format(sys.argv[0],r[1]))
 	if r[0] == 3:
-		with open("runsquashfs.sh","w") as f:
+		with open("./squashfs-root/tmp/runsquashfs.sh","w") as f:
 			f.write("#!/bin/bash\n")
+			r = r[1:]
 			for rr in r:
 				if rr[0] == 1:
 					f.write("sudo apt-get install  -y {}\n".format(rr[1]))
@@ -173,19 +164,15 @@ for r in flow:
 				if rr[0] == 2:
 					os.system("cp {} squashfs-root/tmp/".format(rr[1]))
 					print("copy {} to /tmp/".format(rr[1]))
-					f.write("sudo gdebi install -y /tmp/{}\n".format(os.path.basename(rr[1])))
+					f.write("yes | sudo gdebi /tmp/{}\n".format(os.path.basename(rr[1])))
 					print("install {}".format(rr[1]))
-					f.write("rm /tmp/*.deb\n")
+					f.write("rm /tmp/{}\n".format(os.path.basename(rr[1])))
 				if rr[0] == 3:
 					f.write("sudo apt-get remove -y --purge --autoremove {}\n".format(rr[1]))
 					print("purge {}".format(rr[1]))
 				if rr[1] == 4:
 					f.write("{}\n".format(rr[1]))
 					print("run '{}'".format(rr[1]))
-		os.system("cp ./runsquashfs.sh ./squashfs-root/tmp/")
 		os.system("sudo chroot squashfs-root/ bash /tmp/runsquashfs.sh")
-
-os.remove("./runsquashfs.sh")
-os.remove("./squashfs-root/tmp/runsquashfs.sh")
 
 os.system("mksquashfs ./squashfs-root/ filesystem.squashfs -comp xz")
--- build.sh
+++ build.sh
@@ -1,5 +1,100 @@
+#!/bin/bash
+rm *.iso
+URL=https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/`curl -so- https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/ | grep live | grep nonfree | sed -e "s/.*href=\"\(.*\/\)\".*/\1/g"`
+wget ${URL}amd64/iso-hybrid/`curl -so- ${URL}amd64/iso-hybrid/ | grep xfce | grep iso | sed -e "s/.*href=\"\(.*.iso\)\".*/\1/g"`
+rm -r BaseDisk
+mkdir BaseDisk
+mount *.iso BaseDisk/
+rm -rf Simply_Circles_Themes
+git clone https://github.com/ju1464/Simply_Circles_Themes.git
+rm -rf Tela-circle-red
+rm -rf Tela-circle-red-dark
+git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
+./Tela-circle-icon-theme/install.sh -d ./ red
+cp Tela-circle-icon-theme/COPYING ./Tela-circle-red
+cp Tela-circle-icon-theme/AUTHORS ./Tela-circle-red
+cp Tela-circle-icon-theme/COPYING ./Tela-circle-red-dark
+cp Tela-circle-icon-theme/AUTHORS ./Tela-circle-red-dark
+rm -rf Tela-circle-icon-theme
+rm -rf deb
+mkdir deb
+cd deb
+wget https://download.opensuse.org/repositories/home:/antergos/Debian_9.0/amd64/lightdm-webkit2-greeter_2.2.5-1+15.8_amd64.deb
+git clone https://github.com/TMP-tenpura/MicnoSwitcher.git
+cd MicnoSwitcher
+dpkg -b MicnoSwitcher
+mv MicnoSwitcher.deb ..
+cd ..
+rm -rf MicnoSwitcher
+git clone https://github.com/TMP-tenpura/MicnoTheme.git
+cd MicnoTheme
+chmod 755 MicnoTheme/DEBIAN/postinst
+dpkg -b MicnoTheme
+mv MicnoTheme.deb ..
+cd ..
+rm -rf MicnoTheme
+git clone https://github.com/TMP-tenpura/lightdm-theme-micno.git
+cd lightdm-theme-micno
+dpkg -b lightdm-theme-micno
+mv lightdm-theme-micno.deb ..
+cd ..
+rm -rf  lightdm-theme-micno
+git clone https://github.com/TMP-tenpura/MicnoStarter.git
+cd MicnoStarter
+dpkg -b MicnoStarter
+mv MicnoStarter.deb ..
+cd ..
+rm -rf MicnoStarter
+cd ..
+rm -rf kernel
+mkdir kernel
+cd kernel
+mkdir config
+PACK_NAME=`apt-cache search linux-config | tail -n 1 | sed 's/ .*//1'`
+echo $PACK_NAME
+apt download $PACK_NAME
+dpkg -x *.deb config
+xz -dv config/usr/src/$PACK_NAME/config.amd64_none_amd64.xz
+rm *.deb
+git clone -b `echo $PACK_NAME | sed 's/.*-//g'`/master https://github.com/zen-kernel/zen-kernel.git
+mv config/usr/src/$PACK_NAME/config.amd64_none_amd64 ./zen-kernel/.config
+cd zen-kernel
+yes '' | make oldconfig
+make -j 4 bindeb-pkg
+cd ../../
+find `pwd`/kernel -mindepth 1 -maxdepth 1 -name "linux*deb" | sed "s/^/deb /" > micno.Mflow
+cat << EOF >> micno.Mflow
+run sudo apt purge --autoremove \`dpkg -l | grep linux | grep "\\\`ls -1 /boot/ | grep vmlinuz | grep -v zen | sed "s/vmlinuz-\(.*\)-amd64/\1/"\\\`" | sed "s/^ii *\(linux[^ ]*\).*/\1/"\`
+apt wget
+apt curl
+apt plank
+apt grub-customizer
+apt rhythmbox
+apt vlc
+apt fcitx-mozc
+apt python3-distutils
+add ./.config /etc/skel/
+deb ./deb/MicnoStarter.deb
+deb ./deb/MicnoSwitcher.deb
+deb ./deb/MicnoTheme.deb
+deb ./deb/lightdm-theme-micno.deb
+deb ./deb/lightdm-webkit2-greeter_2.2.5-1+15.8_amd64.deb
+add ./Simply_Circles_Themes/GTK-Gnome/Red/Simply_Circles_Red_Dark_Envolved/ /usr/share/themes/
+add ./Tela-circle-red /usr/share/icons/
+add ./Tela-circle-red-dark /usr/share/icons/
+pge uim
+pge xterm*
+pge ibus*
+pge anthy*
+pge xiterm+thai
+pge mlterm*
+pge atril
+pge exfalso
+pge parole
+pge firefox-esr
+pge goldendict
+pge khmerconverter
+EOF
 sudo python3 build.py \
 	-f ./micno.Mflow\
-	-s /home/micno/debian/live/filesystem.squashfs\
-	-t /home/micno/micnokit/desktop-base/micno-theme\
-	-p /home/micno/micnokit/micno-theme\
+	-s ./BaseDisk/live/filesystem.squashfs
